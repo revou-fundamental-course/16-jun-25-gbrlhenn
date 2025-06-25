68138e78fcfa9519e4a51657c282228d6ef56e64
Gabriel Terok <henrieterok13@gmail.com>
2025-06-25 20:50:44 +0700

    img/segitiga.png

--- js/script.js
+++ js/script.js
@@ -25,3 +25,23 @@ function resetKeliling() {
     document.getElementById("sisiC").value = "";
     document.getElementById("hasilKeliling").innerText = "";
 }
+
+function hitungLuas() {
+  const alas = parseFloat(document.getElementById("alas").value);
+  const tinggi = parseFloat(document.getElementById("tinggi").value);
+  const hasil = document.getElementById("hasilLuas");
+
+  if (isNaN(alas) || isNaN(tinggi)) {
+    hasil.innerText = "Masukkan nilai alas dan tinggi yang valid!";
+    return;
+  }
+
+  const luas = 0.5 * alas * tinggi;
+  hasil.innerText = `Luas segitiga adalah ${luas}`;
+}
+
+function resetForm() {
+  document.getElementById("alas").value = "";
+  document.getElementById("tinggi").value = "";
+  document.getElementById("hasilLuas").innerText = "";
+}
